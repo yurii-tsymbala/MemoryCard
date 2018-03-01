8c813102ed25fbf91fcd24d0d3b324cfb4d758ea
Yurii Tsymbala <yurafloka@yurii-tsymbala.local>
2018-03-01 17:59:37 +0200

    Updated logic after tap on locked levels

--- MemoryCard/Controllers/MenuViewController.swift
+++ MemoryCard/Controllers/MenuViewController.swift
@@ -27,6 +27,10 @@ class MenuViewController: UIViewController {
     
     var imagePackName = "Pokemons"
     
+    var titles = ""
+    
+    var message = ""
+    
     @IBOutlet weak var imageLabel: UILabel!
     
     @IBOutlet weak var imagePackCollectionView: UICollectionView!
@@ -61,6 +65,13 @@ class MenuViewController: UIViewController {
         }
     }
     
+    func showAlert(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert,animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "NumberOfCard" {
             let numberOfCardsFromCell = segue.destination as! GameViewController
@@ -76,18 +87,17 @@ extension MenuViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         if collectionView == self.imagePackCollectionView {
-            
             // Changes color of imageCardCell
-            imagePackCollectionView.cellForItem(at: indexPath)?.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-            
+            imagePackCollectionView.cellForItem(at: indexPath)?.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
             // Saves name of imagePack from imagePackCell
             imagePackName = String(imagesPackLabel[indexPath.item])
             print("\(imagePackName) stickerPack Selected ")
         } else {
-            
-            // Changes color of levelCardCell
-            levelPackCollectionView.cellForItem(at: indexPath)?.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-            
+            if indexPath.row > (results.count) {
+                titles = "No access"
+                message = "Pass previous levels"
+                showAlert(titles, message)
+            }
             // Saves numberOfCards from levelPackCell
             cardNumber = Int(levelsPack[indexPath.item])!
             print("NumberOfCards in Level = \(cardNumber)")
@@ -114,22 +124,28 @@ extension MenuViewController: UICollectionViewDataSource {
             let cell: ImagePackCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImagePackCell", for: indexPath) as! ImagePackCollectionViewCell
             cell.imagePackView.image = imagesPack[indexPath.item]
             cell.nameOfImagePackView.text = imagesPackLabel[indexPath.item]
-            
-            // Design of imagePackCell
-            cell.alpha = 0
-            cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
-            UIView.animate(withDuration: 0.6, animations: { () -> Void in
-                cell.alpha = 1
-                cell.layer.transform = CATransform3DScale(CATransform3DIdentity, 1, 1, 1)
-            })
             return cell
         } else {
-            
             let cell: LevelPackCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "LevelPackCell", for: indexPath) as! LevelPackCollectionViewCell
             cell.numberOfLevel.text = levelsPack[indexPath.item]
             cell.scoreOfLevel.text = "Best score: -- tries"
             cell.timeOfLevel.text = "Best time: -- sec"
             
+            if indexPath.row < (results.count) {
+                cell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            } else if indexPath.row == results.count {
+                cell.backgroundColor = #colorLiteral(red: 0.7972514629, green: 0.6857745647, blue: 0.05130522698, alpha: 1)
+            } else {
+                cell.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            }
+            cell.layer.cornerRadius = 50
+            cell.layer.borderWidth = 3
+            cell.alpha = 0
+            cell.layer.transform = CATransform3DMakeScale(0.5, 0.5, 0.5)
+            UIView.animate(withDuration: 0.8, animations: { () -> Void in
+                cell.alpha = 1
+                cell.layer.transform = CATransform3DScale(CATransform3DIdentity, 1, 1, 1)
+            })
             for result in results {
                 let tryResult = result.value(forKey: "tries") ?? 0
                 let cardsResult = "\(result.value(forKey: "cardsNumber") ?? "0")"
@@ -139,27 +155,6 @@ extension MenuViewController: UICollectionViewDataSource {
                     cell.timeOfLevel.text = "Best time: \(timeResult) sec"
                 }
             }
-            
-            cell.alpha = 0
-            cell.layer.transform = CATransform3DMakeScale(0.5, 0.5, 0.5)
-            UIView.animate(withDuration: 0.8, animations: { () -> Void in
-                cell.alpha = 1
-                cell.layer.transform = CATransform3DScale(CATransform3DIdentity, 1, 1, 1)
-            })
-            
-            if indexPath.row < (results.count) {
-                cell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-                cell.isUserInteractionEnabled = true
-            } else if indexPath.row == results.count{
-                cell.isUserInteractionEnabled = true
-                cell.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
-            }  else {
-                cell.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-                cell.isUserInteractionEnabled = false
-            }
-            cell.layer.cornerRadius = 50
-            cell.layer.borderWidth = 3
-            
             return cell
         }
     }
